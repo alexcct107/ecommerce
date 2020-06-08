557417effbbf58159224ead771a632068a50ba5f
Ivan Carrasco <alexcct@hotmail.com>
2020-06-08 10:57:29 -0400

    Adding authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  root to: "home#index"
+
+  authenticate :admin do
   resources :products
   resources :categories
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
